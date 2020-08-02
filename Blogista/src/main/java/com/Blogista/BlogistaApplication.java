b2d91fc2f7a1202f149a8a2604d4521b2b86e873
NOMAN AKHTAR <NOMAN AKHTAR@DESKTOP-212T74E>
2020-08-03 01:01:18 +0530

    initial commit

---
+++ Blogista/src/main/java/com/Blogista/BlogistaApplication.java
@@ -0,0 +1,13 @@
+package com.Blogista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogistaApplication.class, args);
+	}
+
+}
---
+++ Blogista/src/test/java/com/Blogista/BlogistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blogista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
